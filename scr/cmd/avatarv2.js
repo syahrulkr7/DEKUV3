868740518413b969a5eb911dd76c4567304c2eda
Churchill <141407524+churchillitos@users.noreply.github.com>
2024-08-19 18:17:30 +0800

    Update avatarv2.js

--- scr/cmd/avatarv2.js
+++ scr/cmd/avatarv2.js
@@ -6,22 +6,27 @@ module.exports = {
   config: {
     name: "avatarv2",
     description: "Generate avatar v2",
-    usage: "[avatarv2 <id> | <bgtext> | <signature> | <color>]",
-    cooldown: 5, 
-    accessableby: 0, 
-    category: "image", 
-    prefix: false 
+    usage: "avatarv2 <id> | <bgtext> | <signature> | <color>",
+    cooldown: 5,
+    accessableby: 0,
+    category: "image",
+    prefix: false,
+    author: "Churchill"  
   },
-  start: async function({ api, event, text, reply }) {
+  start: async function({ api, event, text, reply, react }) {
     try {
       const input = text.join(" ");
       const [id, bgtext, signature, color] = input.split(" | ");
 
       if (!id || !bgtext || !signature || !color) {
-        return reply("Please provide all required parameters: id-1to800 | bgtext | signature | color.");
+        return reply("Please provide all required parameters: id (1 to 800) | bgtext | signature | color.");
       }
 
-      const apiUrl = `https://deku-rest-api-3jvu.onrender.com/canvas/avatarv2?id==${encodeURIComponent(id)}&bgtext=${encodeURIComponent(bgtext)}&signature=${encodeURIComponent(signature)}&color=${encodeURIComponent(color)}`;
+  
+      const apiUrl = `${global.deku.ENDPOINT}/canvas/avatarv2?id=${encodeURIComponent(id)}&bgtext=${encodeURIComponent(bgtext)}&signature=${encodeURIComponent(signature)}&color=${encodeURIComponent(color)}`;
+
+       
+      await react('⏳');
 
       reply("Generating avatar, please wait...");
 
@@ -36,6 +41,10 @@ module.exports = {
       }, event.threadID, () => {
         fs.unlinkSync(avatarPath);
       });
+      
+      
+      await react('✅');
+
     } catch (error) {
       console.error('Error:', error);
       reply("An error occurred while processing the request.");
